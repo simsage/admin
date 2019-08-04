94d6e43dcf567d235894588dbd187ed362e09757
Rock de Vocht <rock@peter.conz>
2019-08-04 12:15:35 +1200

    added better comments to settings file

--- app/settings.js
+++ app/settings.js
@@ -1,13 +1,14 @@
 
 const system_config = {
+    // SimSage platform version (used for display only in UI)
     version: '5.0.14',
-    production: false,
+    // api version of api_base
     api_version: 1,
-    theme: 'default',
+    // the service layer end-point, change "localhost:8080" to ...
     api_base: 'http://localhost:8080/api',
-    ws_base: 'http://localhost:8080/ws-api',
+    // the web-site to go to when SimSage logo is clicked on the login page
     web_base: 'http://simsage.nz/',
-    // date pickers display format
+    // date picker display format
     date_format: 'yyyy/MM',
 };
 
