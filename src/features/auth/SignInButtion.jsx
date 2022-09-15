43b149d27ad8b63fc0eb3c5cc2f87a65f738da14
Rock <rock@simsage.co.uk>
2022-09-15 21:28:56 +0100

    added software licenses

--- src/features/auth/SignInButtion.jsx
+++ src/features/auth/SignInButtion.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import {useMsal} from "@azure/msal-react";
 import {loginRequest} from "./authConfig";
 import {Button} from "bootstrap";
 
 import "../../css/sign-in.css";
+import OpenSourceSoftwareLicenses from "../home/OpenSourceSoftwareLicenses";
 
 
 function handleLogin(instance) {
@@ -15,6 +16,12 @@ function handleLogin(instance) {
 export const SignInButton = () => {
     const image = "../images/simsage-logo-no-strapline.svg";
     const { instance } = useMsal()
+
+    const [show_license, setShowLicense] = useState(false);
+
+    function toggleLicense() {
+        setShowLicense(!show_license)
+    }
 
     return (
         <div>
@@ -29,17 +36,21 @@ export const SignInButton = () => {
                     <div className="sign-in-title">SimSage Mind sign in</div>
 
                     <span className="form-group sign-in-button" title="Sign into the SimSage mind">
-                        <button className="btn btn-primary" onClick={() => handleLogin(instance)}>
+                        <button className="btn btn-primary" onClick={() => { setShowLicense(false); handleLogin(instance) }}>
                             Sign in
                         </button>
                     </span>
 
-                    <p className="forgot-password text-right" title="view the open-source licenses used" onClick={() => window.location = '/foss-license'}>
+                    <p className="forgot-password text-right" title="view the open-source licenses used" onClick={() => toggleLicense()}>
                         <span className="forgot-password-link">open-source licenses</span>
                     </p>
 
                 </div>
             </div>
+
+            {show_license &&
+                <OpenSourceSoftwareLicenses/>
+            }
 
         </div>
 
