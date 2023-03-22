9127758f14860f0da3204be1781a6d2a4b130894
Elan Siva <siva@simsage.co.uk>
2023-03-22 07:44:54 +0000

    Gdrive updated

--- src/features/sources/forms/CrawlerGDriveForm.js
+++ src/features/sources/forms/CrawlerGDriveForm.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import Api from "../../../common/api";
 
 export default function CrawlerGDriveForm(props) {
 
@@ -15,6 +16,15 @@ export default function CrawlerGDriveForm(props) {
     const l_form_data = props.form_data;
 
     const [has_error,setError] = useState()
+    const [time_now, setTimeNow] = useState(specific_json.deltaIndicator)
+
+    const time2020 = 1577836800;
+    let date_time_str = "complete crawl";
+
+    if (specific_json.deltaIndicator > time2020){
+        date_time_str = Api.toPrettyDateTime(new Date(time_now * 1));
+    }
+
 
 
     //update local variable specific_json when data is changed
@@ -31,6 +41,10 @@ export default function CrawlerGDriveForm(props) {
     }, [specific_json])
 
 
+    function handleSetTimeNow() {
+        setTimeNow(Math.floor(Date.now()))
+    }
+
     if (has_error) {
         return <h1>CrawlerFileForm.js: Something went wrong.</h1>;
     }
@@ -39,106 +53,106 @@ export default function CrawlerGDriveForm(props) {
         <div className="crawler-page">
 
             <div className="form-group">
+                    <span className="office-manual-box">
+                    <a href="resources/simsage-google-drive-setup.pdf" id="dlGDrive" target="_blank" title="download the SimSage Google-drive setup guide">
+                            <span className="instructions-label">instructions</span>
+                            <img src="../images/pdf-icon.png" alt="google-drive setup guide" className="image-size" />
+                        </a>
+                    </span>
+            </div>
+
+            <div className="form-group">
+                <div className="full-column-2">
+                    <span className="label-right-top">JSON key contents</span>
+                    <span className="bigger-text">
+                            <form>
+                                <textarea className="textarea-width"
+                                          rows="7"
+                                          placeholder="the Google JSON key identifying the service account to use to access and impersonate user-drive data.  Leave empty if you've already set this value previously and don't want to change it."
+                                          value={specific_json.json_key_file}
+                                          onChange={(event) => {setData({json_key_file: event.target.value})}}
+                                />
+                            </form>
+                        </span>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <div className="full-column-2">
+                    <span className="label-right-top">user list</span>
+                    <span className="bigger-text">
+                            <form>
+                                <textarea className="textarea-width"
+                                          rows="3"
+                                          placeholder="a list of user email-addresses separated by commas whose drives to crawl (required!)"
+                                          value={specific_json.drive_user_csv}
+                                          onChange={(event) => {setData({drive_user_csv: event.target.value})}}
+                                />
+                            </form>
+                        </span>
+                </div>
+            </div>
+
+            <div className="form-group">
                     <span className="left-column">
-                        <span className="small-label-right">user name</span>
+                        <span className="small-label-right">drive id</span>
                         <span className="big-text">
                             <form>
                                 <input type="text" className="form-control"
-                                       autoFocus={true}
-                                       placeholder="user name"
-                                       value={specific_json.username}
-                                       onChange={(event) => {setData({username: event.target.value})}}
+                                       placeholder="drive id (optional)"
+                                       value={specific_json.drive_id}
+                                       onChange={(event) => {setData({drive_id: event.target.value})}}
                                 />
                             </form>
                         </span>
                     </span>
                 <span className="right-column">
-                        <span className="small-label-right">password</span>
-                        <span className="big-text">
-                            <form>
-                                <input type="password" className="form-control"
-                                       placeholder="password"
-                                       value={specific_json.password}
-                                       onChange={(event) => {setData({password: event.target.value})}}
-                                />
-                            </form>
-                        </span>
-                    </span>
-            </div>
-
-
-            <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">domain name</span>
-                        <span className="big-text">
-                            <form>
-                                <input type="text" className="form-control"
-                                       placeholder="domain name"
-                                       value={specific_json.domain}
-                                       onChange={(event) => {setData({domain: event.target.value})}}
-                                />
-                            </form>
-                        </span>
-                    </span>
-                <span className="right-column">
-                        <span className="small-label-right">fqdn</span>
-                        <span className="big-text">
-                            <form>
-                                <input type="text" className="form-control"
-                                       placeholder="e.g.  simsage.ai  (this will form your user's email addresses, eg. account-name@simsage.ai)"
-                                       value={specific_json.fqdn}
-                                       onChange={(event) => {setData({fqdn: event.target.value})}}
-                                />
-                            </form>
-                        </span>
                     </span>
             </div>
 
             <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">server</span>
-                        <span className="big-text">
-                            <form>
-                                <input type="text" className="form-control"
-                                       placeholder="server (hostname or ip-address)"
-                                       value={specific_json.server}
-                                       onChange={(event) => {setData({server: event.target.value})}}
-                                />
-                            </form>
-                        </span>
-                    </span>
-                <span className="right-column">
-                        <span className="small-label-right">share name</span>
-                        <span className="big-text">
-                            <form>
-                                <input type="text" className="form-control"
-                                       placeholder="share name"
-                                       value={specific_json.shareName}
-                                       onChange={(event) => {setData({shareName: event.target.value})}}
-                                />
-                            </form>
-                        </span>
-                    </span>
+                <div className="full-column-2" style={{marginLeft: '170px', width: '400px'}}>
+                    <div style={{float: 'left'}} title="Check this box if you only want to crawl Google Sites from this drive.">
+                        <input type="checkbox"
+                               checked={specific_json.sites_only}
+                               onChange={(event) => { setData({sites_only: event.target.checked}); }}
+                               value="Crawl only Google site data from these drives?"
+                        />
+                        <span className="label-left">Crawl only Google site data from these drives?</span>
+                    </div>
+                </div>
             </div>
-
-
 
             <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">share path</span>
-                        <span className="big-text">
-                            <form>
-                                <input type="text" className="form-control"
-                                       placeholder="path inside share (optional)"
-                                       value={specific_json.sharePath}
-                                       onChange={(event) => {setData({sharePath: event.target.value})}}
-                                />
-                            </form>
+                <div className="full-column-2">
+                    <span className="label-right-top">time to check from</span>
+                    <span className="big-text">
+                            <table>
+                                <tbody>
+                                <tr>
+                                    {/*<td>*/}
+                                    {/*    <input type="text" className="form-control dropbox-text-width"*/}
+                                    {/*           spellCheck={false}*/}
+                                    {/*           style={{width: "200px", marginRight: "10px"}}*/}
+                                    {/*           placeholder="time to check from"*/}
+                                    {/*           value={time_now}*/}
+                                    {/*           onChange={(event) => {setData({deltaIndicator: event.target.value})}}*/}
+                                    {/*    />*/}
+                                    {/*</td>*/}
+                                    <td>
+                                        <button className="btn bt-sm btn-primary" onClick={() => handleSetTimeNow()}>now</button>
+                                    </td>
+                                    <td>
+                                        <div style={{width: "200px", marginLeft: "30px"}}>{date_time_str}</div>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </span>
-                    </span>
+                </div>
             </div>
-
 
         </div>
-    )
+    );
+
 }
