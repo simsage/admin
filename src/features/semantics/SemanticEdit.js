0083562d9ece519c1328e0c862a8a16a712b6cc7
Cole Holyoake <coleholyoake@live.com>
2023-04-05 21:22:27 +1200

    Semantic modal styling

--- src/features/semantics/SemanticEdit.js
+++ src/features/semantics/SemanticEdit.js
@@ -62,43 +62,46 @@ export function SemanticEdit(){
         <div className="modal user-display" tabIndex="-1" role="dialog" style={{display: "inline", background: "#202731bb"}}>
             <div className={"modal-dialog modal-dialog-centered modal-lg"} role="document">
                 <div className="modal-content">
-                    <div className="modal-header">{selectedSemantic ? "Edit Semantic" : "Add New Semantic"}</div>
-                    <div className="modal-body">
-                        <div className="tab-content">
-
-                            <div className="control-row">
-                                <span className="label-2">Word</span>
-                                <span className="text">
-                                            <form>
-                                                <input type="text" className="form-control"
-                                                       autoFocus={true}
-                                                       autoComplete="false"
-                                                       placeholder="Word"
-                                                       value={word}
-                                                       onChange={(event) => setWord(event.target.value)}
-                                                />
-                                                </form>
-                                        </span>
-                            </div>
-                            <div className="control-row">
-                                <span className="label-2">Semantics</span>
-                                <span className="text">
-                                            <form>
-                                                <input type="text" className="form-control"
-                                                       autoFocus={true}
-                                                       autoComplete="false"
-                                                       placeholder="Semantic Words"
-                                                       value={semanticType}
-                                                       onChange={(event) => setSemanticType(event.target.value)}
-                                                />
-                                                </form>
-                                        </span>
+                <div className="modal-header px-5 pt-4 bg-light">
+                    <h4 className="mb-0">{selectedSemantic ? "Edit Semantic" : "New Semantic"}</h4>
+                    </div>
+                    <div className="modal-body p-0">
+                        <div className="tab-content px-5 py-4 overflow-auto">
+                            <div className="row mb-3">
+                                <div className="control-row col-6">
+                                    <span className="label-2 small">Word</span>
+                                    <span className="text">
+                                        <form>
+                                            <input type="text" className="form-control"
+                                                    autoFocus={true}
+                                                    autoComplete="false"
+                                                    placeholder="e.g. Wellington"
+                                                    value={word}
+                                                    onChange={(event) => setWord(event.target.value)}
+                                            />
+                                        </form>
+                                    </span>
+                                </div>
+                                <div className="control-row col-6">
+                                    <span className="label-2 small">Semantic</span>
+                                    <span className="text">
+                                        <form>
+                                            <input type="text" className="form-control"
+                                                    autoFocus={true}
+                                                    autoComplete="false"
+                                                    placeholder="e.g. City"
+                                                    value={semanticType}
+                                                    onChange={(event) => setSemanticType(event.target.value)}
+                                            />
+                                        </form>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="modal-footer">
-                        <button className="btn btn-primary btn-block" onClick={(e) => handleClose(e)}>Cancel</button>
-                        <button className="btn btn-primary btn-block" onClick={(e) => handleSave(e)}>Save</button>
+                    <div className="modal-footer px-5 pb-4">
+                        <button className="btn btn-white btn-block px-4" onClick={(e) => handleClose(e)}>Cancel</button>
+                        <button className="btn btn-primary btn-block px-4" onClick={(e) => handleSave(e)}>Save</button>
                     </div>
 
                 </div>
