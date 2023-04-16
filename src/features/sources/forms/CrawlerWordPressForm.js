f1b0d0ce4a2e3412f45c27b57778fef67ddc98c6
Cole Holyoake <coleholyoake@live.com>
2023-04-16 18:10:22 +1200

    Further Wordpress crawler styling

--- src/features/sources/forms/CrawlerWordPressForm.js
+++ src/features/sources/forms/CrawlerWordPressForm.js
@@ -58,33 +58,35 @@ export default function CrawlerWordPressForm(props) {
             <div className="row justify-content-center">
                 <div className="col-6">
                     <div class="alert alert-warning small py-2" role="alert">
-                    The WordPress crawler is an external entity controlled by the SimSage WordPress plugin.<br/><br/>
-                    As such there are no properties to configure here, nor is there a schedule for SimSage to work to.
+                        <p className="mb-2">The WordPress crawler is an external entity controlled by the SimSage WordPress plugin.</p>
+                        <p>
+                        As such there are no properties to configure here, nor is there a schedule for SimSage to work to.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div className="col-6 wp-form">
-                
+            <div className="row justify-content-center">
+                <div className="col-6 wp-form">
+                    {specific_json.source_id > 0 &&
+                        <div className="export-wp d-flex align-items-center mb-4">
+                            <label className="small me-3">Export the contents of this source as a WordPress GZip Archive</label>
+                            <button className="btn btn-primary btn-block"
+                                    onClick={() => exportArchive()}>Export</button>
+                        </div>
+                    }
 
-                {specific_json.source_id > 0 &&
-                    <div className="export-wp">
-                        <div>export the contents of this source as a WordPress GZip Archive</div>
-                        <button className="btn btn-primary btn-block"
-                                onClick={() => exportArchive()}>Export WordPress Archive</button>
-                    </div>
-                }
+                    {specific_json.source_id > 0 &&
+                        <div className="import-wp d-flex align-items-center">
+                            <lable className="small me-3">Import the contents of WordPress GZip Archive into this source</lable>
+                            <RestoreWPArchive doUpload={(data) => restore(data)}
+                                            organisationId={props.source.organisationId}
+                                            kbId={props.source.kb_id}
+                                            sourceId={specific_json.source_id}
+                                            onError={(err) => this.props.setError("Error", err)} />
+                        </div>
+                    }
 
-                {specific_json.source_id > 0 &&
-                    <div className="import-wp">
-                        <div className="import-text">import the contents of  WordPress GZip Archive into this source</div>
-                        <RestoreWPArchive doUpload={(data) => restore(data)}
-                                          organisationId={props.source.organisationId}
-                                          kbId={props.source.kb_id}
-                                          sourceId={specific_json.source_id}
-                                          onError={(err) => this.props.setError("Error", err)} />
-                    </div>
-                }
-
+                </div>
             </div>
         </div>
     )
