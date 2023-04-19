be4b66c6499057e94a82faec258153c59736681e
Cole Holyoake <coleholyoake@live.com>
2023-04-19 21:35:02 +1200

    File SMB crawler styling

--- src/features/sources/forms/CrawlerFileForm.js
+++ src/features/sources/forms/CrawlerFileForm.js
@@ -36,109 +36,84 @@ export default function CrawlerFileForm(props) {
     }
 
     return (
-        <div className="crawler-page">
-
-            <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">user name</span>
-                        <span className="big-text">
-                            
-                                <input type="text" className="form-control"
-                                       autoFocus={true}
-                                       placeholder="user name"
-                                       value={specific_json.username}
+        <div className="tab-content px-5 py-4 overflow-auto">
+            <div className="row mb-4">
+                <div className="form-group col-4">
+                    <label className="small">Username</label>
+                    <form>
+                        <input type="text" className="form-control"
+                            placeholder=""
+                            autoFocus={true}
+                            value={specific_json.username}
                                        onChange={(event) => {setData({username: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
-                <span className="right-column">
-                        <span className="small-label-right">password</span>
-                        <span className="big-text">
-                            
-                                <input type="password" className="form-control"
-                                       placeholder="password"
-                                       value={specific_json.password}
-                                       onChange={(event) => {setData({password: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
+                        />
+                    </form>
+                </div>
+                <div className="form-group col-4">
+                    <label className="small">Password</label>
+                    <form>
+                        <input type="password" className="form-control"
+                            placeholder="********"
+                            value={specific_json.password}
+                            onChange={(event) => {setData({password: event.target.value})}}
+                        />
+                    </form>
+                </div>
             </div>
-
-
-            <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">domain name</span>
-                        <span className="big-text">
-                            
-                                <input type="text" className="form-control"
-                                       placeholder="domain name"
-                                       value={specific_json.domain}
-                                       onChange={(event) => {setData({domain: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
-                <span className="right-column">
-                        <span className="small-label-right">fqdn</span>
-                        <span className="big-text">
-                            
-                                <input type="text" className="form-control"
-                                       placeholder="e.g.  simsage.ai  (this will form your user's email addresses, eg. account-name@simsage.ai)"
-                                       value={specific_json.fqdn}
-                                       onChange={(event) => {setData({fqdn: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
+            <div className="row mb-4">
+                <div className="form-group col-4">
+                    <label className="small">Domain name</label>
+                    <form>
+                        <input type="text" className="form-control"
+                            placeholder=""
+                            value={specific_json.domain}
+                                onChange={(event) => {setData({domain: event.target.value})}}
+                        />
+                    </form>
+                </div>
+                <div className="form-group col-8">
+                    <label className="small">FQDN</label>
+                    <form>
+                        <input type="text" className="form-control"
+                            placeholder="e.g. simsage.ai  (this will form your user's email addresses, eg. account-name@simsage.ai)"
+                            value={specific_json.fqdn}
+                                onChange={(event) => {setData({fqdn: event.target.value})}}
+                        />
+                    </form>
+                </div>
             </div>
-
-            <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">server</span>
-                        <span className="big-text">
-                            
-                                <input type="text" className="form-control"
-                                       placeholder="server (hostname or ip-address)"
-                                       value={specific_json.server}
+            <div className="row mb-4">
+                <div className="form-group col-4">
+                    <label className="small">Server</label>
+                    <form>
+                        <input type="text" className="form-control"
+                            placeholder="(hostname or IP-address)"
+                            value={specific_json.server}
                                        onChange={(event) => {setData({server: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
-                <span className="right-column">
-                        <span className="small-label-right">share name</span>
-                        <span className="big-text">
-                            
-                                <input type="text" className="form-control"
-                                       placeholder="share name"
-                                       value={specific_json.shareName}
+                        />
+                    </form>
+                </div>
+                <div className="form-group col-4">
+                    <label className="small">Share name</label>
+                    <form>
+                        <input type="text" className="form-control"
+                            placeholder=""
+                            value={specific_json.shareName}
                                        onChange={(event) => {setData({shareName: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
-            </div>
-
-
-
-            <div className="form-group">
-                    <span className="left-column">
-                        <span className="small-label-right">share path</span>
-                        <span className="big-text">
-                            
-                                <input type="text" className="form-control"
-                                       placeholder="path inside share (optional)"
-                                       value={specific_json.sharePath}
+                        />
+                    </form>
+                </div>
+                <div className="form-group col-4">
+                    <label className="small">Share path</label>
+                    <form>
+                        <input type="text" className="form-control"
+                            placeholder="path inside share (optional)"
+                            value={specific_json.sharePath}
                                        onChange={(event) => {setData({sharePath: event.target.value})}}
-                                />
-                            
-                        </span>
-                    </span>
+                        />
+                    </form>
+                </div>
             </div>
-
-
         </div>
     )
 }
