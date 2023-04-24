33483da6048561eb7063d80108a454c39adc3083
Elan Siva <siva@simsage.co.uk>
2023-04-24 15:13:02 +0100

    exchange crawler

--- src/features/sources/forms/CrawlerExchange365Form.js
+++ src/features/sources/forms/CrawlerExchange365Form.js
@@ -43,7 +43,7 @@ export default function CrawlerExchange365Form(props) {
                 <div className="col-9">
                     <div className="row mb-4">
                         <div className="form-group col-6">
-                            <label className="small">Domain name</label>
+                            <label className="small">Tenant ID</label>
                             <form>
                                 <input type="text" className="form-control"
                                     placeholder=""
