1eb4b827327d84ef44ad3f505052893748f0c240
Rock <rock@simsage.co.uk>
2023-05-09 10:22:41 +0100

    set version to 7.7.4

--- public/settings.js
+++ public/settings.js
@@ -1,7 +1,7 @@
 
 window.ENV = {
     // SimSage platform version (used for display only in UI)
-    version: '7.7.5',
+    version: '7.7.4',
     // api version of api_base
     api_version: 1,
     // is this a production build or not?
