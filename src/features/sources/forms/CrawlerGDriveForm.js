732a9d41e57820c51ac540b8d49e93f528bcb379
Rock <rock@simsage.co.uk>
2023-06-17 10:56:36 +0100

    added gdrive and api reset

--- src/features/sources/forms/CrawlerGDriveForm.js
+++ src/features/sources/forms/CrawlerGDriveForm.js
@@ -12,20 +12,12 @@ export default function CrawlerGDriveForm(props) {
     const specific_json_from_form_data = (props.form_data && props.form_data.specificJson) ? props.form_data.specificJson : selected_source.specificJson ? selected_source.specificJson : "{}"
     const [specific_json, setSpecificJson] = useState(JSON.parse(specific_json_from_form_data))
     const l_form_data = props.form_data;
-    const [time_now, setTimeNow] = useState(specific_json.deltaIndicator?specific_json.deltaIndicator:0)
 
-    const time2020 = 1577836800;
-    let date_time_str = "complete crawl";
-
-    if (time_now > time2020){
-        date_time_str = Api.toPrettyDateTime(new Date(time_now * 1));
-    }
-
-
+    const time2020 = 1577836800000;
 
     //update local variable specific_json when data is changed
     function setData(data) {
-        setSpecificJson({...specific_json,...data})
+        setSpecificJson({...specific_json, ...data})
     }
 
 
@@ -39,8 +31,16 @@ export default function CrawlerGDriveForm(props) {
 
     function handleSetTimeNow() {
         const temp_time_now = Math.floor(Date.now())
-        setTimeNow(temp_time_now)
-        setData({deltaIndicator:temp_time_now});
+        setData({deltaIndicator: temp_time_now});
+    }
+
+    function handleResetTime() {
+        setData({deltaIndicator: time2020});
+    }
+
+    let date_time_str = "complete crawl";
+    if (specific_json.deltaIndicator > time2020) {
+        date_time_str = Api.toPrettyDateTime(new Date(specific_json.deltaIndicator));
     }
 
     return (
@@ -50,14 +50,14 @@ export default function CrawlerGDriveForm(props) {
                     <div className="row mb-4">
                         <div className="form-group col-12">
                             <label className="small">
-                            JSON key contents
+                                JSON key contents
                             </label>
                             <textarea className="form-control"
-                                disabled={specific_json.crawlAllSites}
-                                placeholder="The Google JSON key identifying the service account to use to access and impersonate user-drive data.Leave empty if you've already set this value previously and don't want to change it."
-                                rows={5}
-                                value={specific_json.json_key_file}
-                                          onChange={(event) => {setData({json_key_file: event.target.value})}}
+                                      disabled={specific_json.crawlAllSites}
+                                      placeholder="The Google JSON key identifying the service account to use to access and impersonate user-drive data.Leave empty if you've already set this value previously and don't want to change it."
+                                      rows={5}
+                                      value={specific_json.json_key_file}
+                                      onChange={(event) => {setData({json_key_file: event.target.value})}}
                             />
                         </div>
                     </div>
@@ -69,10 +69,10 @@ export default function CrawlerGDriveForm(props) {
                                 <span className="text-danger ms-1">Required</span></span>
                             </label>
                             <textarea className="form-control"
-                                placeholder=""
-                                rows={3}
-                                value={specific_json.drive_user_csv}
-                                          onChange={(event) => {setData({drive_user_csv: event.target.value})}}
+                                      placeholder=""
+                                      rows={3}
+                                      value={specific_json.drive_user_csv}
+                                      onChange={(event) => {setData({drive_user_csv: event.target.value})}}
                             />
                         </div>
                     </div>
@@ -81,9 +81,9 @@ export default function CrawlerGDriveForm(props) {
                             <label className="small">Drive ID</label>
                             <form>
                                 <input type="text" className="form-control"
-                                    placeholder="Optional"
-                                    autoFocus={true}
-                                    value={specific_json.drive_id}
+                                       placeholder="Optional"
+                                       autoFocus={true}
+                                       value={specific_json.drive_id}
                                        onChange={(event) => {setData({drive_id: event.target.value})}}
                                 />
                             </form>
@@ -92,10 +92,10 @@ export default function CrawlerGDriveForm(props) {
                     <div className="row mb-4">
                         <div className="col-6">
                             <div className="form-check form-switch">
-                                <input className="form-check-input" type="checkbox"     
-                                    checked={specific_json.sites_only}
-                                    onChange={(event) => { setData({sites_only: event.target.checked}); }}
-                                    title="Crawl only Google site data from these Drives"/>
+                                <input className="form-check-input" type="checkbox"
+                                       checked={specific_json.sites_only}
+                                       onChange={(event) => { setData({sites_only: event.target.checked}); }}
+                                       title="Crawl only Google site data from these Drives"/>
                                 <label className="form-check-label small" for="enableOperator">Crawl only Google site data from these Drives</label>
                             </div>
                         </div>
@@ -105,12 +105,12 @@ export default function CrawlerGDriveForm(props) {
                             <label className="small">Time to check from</label>
                             <div className="d-flex align-items-center">
                                 <input type="text" className="form-control"
-                                    placeholder="Optional"
-                                    autoFocus={true}
-                                    value={time_now}
-                                               onChange={(event) => {setData({deltaIndicator: event.target.value})}}
+                                       placeholder="Optional"
+                                       value={specific_json.deltaIndicator}
+                                       onChange={(event) => {setData({deltaIndicator: event.target.value})}}
                                 />
                                 <div className="btn bt-sm btn-primary ms-2" onClick={() => handleSetTimeNow()}>Now</div>
+                                <div className="btn bt-sm btn-primary ms-2" onClick={() => handleResetTime()}>Reset</div>
                                 <span className="small text-nowrap ms-3 text-capitalize">{date_time_str}</span>
                             </div>
                         </div>
@@ -118,9 +118,9 @@ export default function CrawlerGDriveForm(props) {
                 </div>
                 <div className="col-2 offset-1">
                     <a href="resources/simsage-google-drive-setup.pdf" id="dlGDrive" target="_blank"
-                    title="Download the SimSage Google Drive setup guide" className="d-flex align-items-center flex-column text-center small alert alert-primary small py-2">
-                    <BsFilePdf size={25}/>
-                    <span className="me-2 mt-2"></span>Google Drive <br/>Setup Guide 
+                       title="Download the SimSage Google Drive setup guide" className="d-flex align-items-center flex-column text-center small alert alert-primary small py-2">
+                        <BsFilePdf size={25}/>
+                        <span className="me-2 mt-2"></span>Google Drive <br/>Setup Guide
                     </a>
                 </div>
             </div>
