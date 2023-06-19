c536a3f80630b70a0be096e0744abf98f6a2eb51
Elan Siva <siva@simsage.co.uk>
2023-06-19 15:20:08 +0100

    user form fix defaults

--- src/features/users/UsersHome.js
+++ src/features/users/UsersHome.js
@@ -15,65 +15,84 @@ import api from "../../common/api";
 import {UserEditV2} from "./UserEditV2";
 import {UserPasswordResetForm} from "./UserPasswordResetForm";
 
-export function UsersHome(){
+export function UsersHome() {
     const user_roles = useSelector((state) => state.usersReducer.roles);
     const [page, setPage] = useState(api.initial_page);
     const [page_size, setPageSize] = useState(api.initial_page_size);
 
     const [selectedUser, setSelectedUser] = useState();
-    const [searchFilter,setSearchFilter] = useState('');
-    const [userFilter,setUserFilter] = useState('all-users');
+    const [searchFilter, setSearchFilter] = useState('');
+    const [userFilter, setUserFilter] = useState('all-users');
 
     const theme = null;
     const dispatch = useDispatch();
 
     const user_account = useSelector((state) => state.authReducer.user)
     const user_list = useSelector((state) => state.usersReducer.user_list)
-    const session = useSelector((state)=>state.authReducer.session)
-    const selected_organisation_id = useSelector((state)=>state.authReducer.selected_organisation_id)
+    const session = useSelector((state) => state.authReducer.session)
+    const selected_organisation_id = useSelector((state) => state.authReducer.selected_organisation_id)
     const load_data = useSelector((state) => state.usersReducer.data_status)
     const count = useSelector((state) => state.usersReducer.count)
 
     const isAdmin = hasRole(user_account, ['admin']);
     const isManager = hasRole(user_account, ['manager']);
 
-    useEffect(()=>{
-        dispatch(getUserListPaginated({session_id:session.id, organization_id:selected_organisation_id,page:page,page_size:page_size,filter:null}))
+    const show_user_form = useSelector((state) => state.usersReducer.show_user_form);
+    const show_password_reset_form = useSelector((state) => state.usersReducer.show_password_reset_form);
+    const show_delete_form = useSelector((state) => state.usersReducer.show_delete_form)
+    const show_user_bulk_form = useSelector((state) => state.usersReducer.show_user_bulk_form)
+
+    useEffect(() => {
+        dispatch(getUserListPaginated({
+            session_id: session.id,
+            organization_id: selected_organisation_id,
+            page: page,
+            page_size: page_size,
+            filter: null
+        }))
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[load_data === "load_now", page, page_size])
+    }, [load_data === "load_now", page, page_size])
 
     function handleSearchTextKeydown(e) {
-        if (e.key === "Enter"  && selected_organisation_id) {
-            dispatch(getUserListPaginated({session_id:session.id, organization_id:selected_organisation_id,page:page,page_size:page_size,filter:searchFilter === '' ? null : searchFilter}))
+        if (e.key === "Enter" && selected_organisation_id) {
+            dispatch(getUserListPaginated({
+                session_id: session.id,
+                organization_id: selected_organisation_id,
+                page: page,
+                page_size: page_size,
+                filter: searchFilter === '' ? null : searchFilter
+            }))
             setSearchFilter('');
         }
     }
 
     function handleSearchTextChange(e) {
-        if (selected_organisation_id) {setSearchFilter(e.target.value);}
+        if (selected_organisation_id) {
+            setSearchFilter(e.target.value);
+        }
     }
 
-    function handleAddNewUser(){
+    function handleAddNewUser() {
         setSelectedUser({});
         dispatch(showAddUserForm(true))
     }
 
     function handleEditUser(u) {
         setSelectedUser(u)
-        dispatch(showEditUserForm({show:true, user_id: u.id}));
+        dispatch(showEditUserForm({show: true, user_id: u.id}));
     }
 
     function handlePasswordReset(u) {
         setSelectedUser(u)
-        dispatch(showPasswordResetForm({show:true, user_id: u.id}));
+        dispatch(showPasswordResetForm({show: true, user_id: u.id}));
     }
 
-    function handleUserBulk(){
+    function handleUserBulk() {
         dispatch(showUserBulkForm());
     }
 
-    function deleteUserAsk(u){
-        dispatch(showDeleteUserAsk({show:true, user_id:u.id}))
+    function deleteUserAsk(u) {
+        dispatch(showDeleteUserAsk({show: true, user_id: u.id}))
     }
 
 
@@ -88,6 +107,7 @@ export function UsersHome(){
         // managers can edit everyone else
         return isManager;
     }
+
     // is this user entitle to edit the user passed in?
     function canDelete(user, signedInUser, isAdmin, isManager) {
         // one cannot delete the signed-in user
@@ -103,27 +123,33 @@ export function UsersHome(){
 
     //Filtering out users according to 'Role' drop down.
     function filterRoles(userRoles, role) {
-        if(role === 'all-users'){return true}
-         return userRoles.some( role_obj => role_obj.organisationId === selected_organisation_id && role_obj.role === role)
+        if (role === 'all-users') {
+            return true
+        }
+        return userRoles.some(role_obj => role_obj.organisationId === selected_organisation_id && role_obj.role === role)
     }
 
-    console.log("user_list",user_list)
+    console.log("user_list", user_list)
 
-    return(
+    return (
         <div className="section px-5 pt-4">
             <div className="d-flex justify-content-beteween w-100 mb-4">
                 <div className="d-flex w-100">
                     <div className="form-group me-2">
-                        <input type="text" placeholder={"Filter..."} value={searchFilter} autoFocus={true} className={"form-control filter-search-input " + theme}
+                        <input type="text" placeholder={"Filter..."} value={searchFilter} autoFocus={true}
+                               className={"form-control filter-search-input " + theme}
                                onKeyDown={(e) => handleSearchTextKeydown(e)}
                                onChange={(e) => handleSearchTextChange(e)}
                         />
                     </div>
                     <div className="form-group me-2">
-                        <select type="text" placeholder={"Filter"} value={userFilter} autoFocus={true} className={"form-select filter-text-width " + theme}
-                                onChange={(e) => {setUserFilter(e.target.value);}}>
+                        <select type="text" placeholder={"Filter"} value={userFilter} autoFocus={true}
+                                className={"form-select filter-text-width " + theme}
+                                onChange={(e) => {
+                                    setUserFilter(e.target.value);
+                                }}>
                             <option value="all-users">All Users</option>
-                            { user_roles && user_roles.map( r => {
+                            {user_roles && user_roles.map(r => {
                                 return (
                                     <option value={r}>{Api.getPrettyRole(r)}</option>
                                 )
@@ -164,19 +190,24 @@ export function UsersHome(){
                         user_list && user_list.map((user) => {
 
                             //user does not have chosen role then skip.
-                            if(!filterRoles(user.roles, userFilter)){return null}
+                            if (!filterRoles(user.roles, userFilter)) {
+                                return null
+                            }
 
                             const editYes = canEdit(user, isAdmin, isManager);
                             const deleteYes = canDelete(user, session, isAdmin, isManager);
 
-                            return <tr key={user.id} >
-                                <td className="pt-3 px-4 pb-3 fw-500">{user.firstName} {user.surname} <span className="fw-light fst-italic pt-2 text-black-50">{user.email === user_account.email ? '(you)' : ""}</span> </td>
+                            return <tr key={user.id}>
+                                <td className="pt-3 px-4 pb-3 fw-500">{user.firstName} {user.surname} <span
+                                    className="fw-light fst-italic pt-2 text-black-50">{user.email === user_account.email ? '(you)' : ""}</span>
+                                </td>
                                 <td className="pt-3 px-4 pb-3 fw-light">{user.email}</td>
                                 <td className="pt-3 px-4 pb-2">
                                     <div className="d-flex flex-wrap">
-                                        { user.roles.map((r,key) => {
-                                            if(r.organisationId !== selected_organisation_id){return null}
-                                            else {
+                                        {user.roles.map((r, key) => {
+                                            if (r.organisationId !== selected_organisation_id) {
+                                                return null
+                                            } else {
                                                 return <div key={key}
                                                             className="small text-capitalize table-pill px-3 py-1 me-2 mb-2 rounded-pill">{api.getPrettyRole(r.role)}</div>
                                             }
@@ -185,9 +216,17 @@ export function UsersHome(){
                                 </td>
                                 <td className="pt-3 px-4 pb-0">
                                     <div className="d-flex  justify-content-end">
-                                        <button className={(editYes)? "btn text-primary btn-sm": "btn btn-secondary disabled"} onClick={() => handlePasswordReset(user)}>Reset password</button>
-                                        <button className={(editYes)? "btn text-primary btn-sm": "btn btn-secondary disabled"} onClick={() => handleEditUser(user)}>Edit</button>
-                                        <button className={(deleteYes)? "btn text-danger btn-sm" : "d-none"} onClick={ () => deleteUserAsk(user)}>Delete</button>
+                                        <button
+                                            className={(editYes) ? "btn text-primary btn-sm" : "btn btn-secondary disabled"}
+                                            onClick={() => handlePasswordReset(user)}>Reset password
+                                        </button>
+                                        <button
+                                            className={(editYes) ? "btn text-primary btn-sm" : "btn btn-secondary disabled"}
+                                            onClick={() => handleEditUser(user)}>Edit
+                                        </button>
+                                        <button className={(deleteYes) ? "btn text-danger btn-sm" : "d-none"}
+                                                onClick={() => deleteUserAsk(user)}>Delete
+                                        </button>
                                     </div>
                                 </td>
                             </tr>
@@ -195,7 +234,7 @@ export function UsersHome(){
                     }
                     </tbody>
                 </table>
-                { user_list &&
+                {user_list &&
                     <Pagination
                         rowsPerPageOptions={[5, 10, 25]}
                         theme={theme}
@@ -210,10 +249,21 @@ export function UsersHome(){
                     />
                 }
             </div>
-            <UserEditV2 />
-            <UserDeleteAsk />
-            <UserBulkForm />
-            <UserPasswordResetForm />
+            {show_user_form &&
+                <UserEditV2/>
+            }
+
+            {show_delete_form &&
+                <UserDeleteAsk/>
+            }
+
+            {show_user_bulk_form &&
+                <UserBulkForm/>
+            }
+
+            {show_password_reset_form &&
+                <UserPasswordResetForm/>
+            }
         </div>
     )
 }
