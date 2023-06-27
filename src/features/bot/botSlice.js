7b1eceae53425ba588cc34bdca36d5a6c404e817
Max Webster <max@simsage.co.uk>
2023-06-27 12:31:11 +0100

    adding success message

--- src/features/bot/botSlice.js
+++ src/features/bot/botSlice.js
@@ -14,6 +14,7 @@ const initialState = {
     show_delete_form: false,
     show_import_form: false,
     show_bot_import_form: false,
+    show_add_info_form: false,
 }
 
 export const loadMindItems = createAsyncThunk(
@@ -131,6 +132,7 @@ const extraReducers = (builder) => {
         .addCase(updateMindItem.fulfilled, (state, action) => {
             state.status = "fulfilled";
             state.data_status = 'load_now';
+            state.show_add_info_form = true;
 
         })
         .addCase(updateMindItem.rejected, (state, action) => {
@@ -210,13 +212,16 @@ const botSlice = createSlice({
         closeBotImportForm:(state) => {
             state.show_bot_import_form = false;
         },
+        showAddInfoForm(state, action) {
+            state.show_add_info_form = action.payload
+        },
     },
     extraReducers
 })
 
 
 export const {
-    showEditMemoryForm, showAddMemoryForm, closeMemoryForm, showDeleteMemoryForm,
+    showAddInfoForm, showEditMemoryForm, showAddMemoryForm, closeMemoryForm, showDeleteMemoryForm,
     closeDeleteForm, closeForm, showBotImportForm, closeBotImportForm
 } = botSlice.actions
 export default botSlice.reducer
