c9f2aa5975e099bf48d48b89b9184b8a6600910a
Max Webster <max@simsage.co.uk>
2023-07-11 09:08:25 +0100

    update error handling botSlice

--- src/features/bot/BotHome.js
+++ src/features/bot/BotHome.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BotList from "./BotList"
+import {BotErrorDialog} from "./BotErrorDialog";
 
 export default function BotHome() {
 
     return (
         <div className="">
             <BotList />
+            <BotErrorDialog/>
         </div>
     )
 
