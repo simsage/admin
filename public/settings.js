fa6ddb9a12af23dcfa5821c67f10b767a5ac43d4
Rock <rock@simsage.co.uk>
2024-09-30 15:50:56 +0100

    updated version

--- public/settings.js
+++ public/settings.js
@@ -1,7 +1,7 @@
 
 window.ENV = {
     // SimSage platform version (used for display only in UI)
-    version: '7.17',
+    version: '7.18',
     // api version of api_base
     api_version: 1,
     // is this a production build or not?
