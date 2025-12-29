f1047855f32b536b4d37db021436933e07df6d57
Rock <rock@simsage.ai>
2025-12-29 16:37:09 +0100

    removed unused files

--- src/features/home/__tests__/fetch.test.js
+++
@@ -1,9 +0,0 @@
-// import {rest} from 'msw'
-// import {setupServer} from 'msw/node'
-// import '@testing-library/jest-dom'
-//
-// const server = setupServer(
-//     rest.get('/greeting', (req, res, ctx) => {
-//         return res(ctx.json({greeting: 'hello there'}))
-//     }),
-// )
